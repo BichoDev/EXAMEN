57e38de434c45b97f594985d802055d94ff9040a
VicenteDiazDev <148249731+BichoDev@users.noreply.github.com>
2023-12-12 23:14:02 -0300

    VISTAS

---
+++ VistaMecanicos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VistaMecanicos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
